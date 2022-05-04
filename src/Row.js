cd32bd03024beaa3792a0b9807e73b0a5c1652c8
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-04 02:11:23 -0700

    fetch data

--- src/Row.js
+++ src/Row.js
@@ -1,7 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "./axios";
 
-const Row = ({title}) => {
+const Row = ({title, fetchURL}) => {
     const [movies, setMovies] = useState([])
+
+    // a snippet of code which runs based on a specific condition or variable
+    useEffect(() => {
+      return () => {
+        // [] RUNONCE WHEN THE ROW LOADS 
+        // [movies] runs everytime the movies change
+        async function fetchData(){
+            const request = await axios.get(fetchURL)
+        }
+        fetchData()
+      }
+    }, [])
+    
 
     return ( 
         <div>
