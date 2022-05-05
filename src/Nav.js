b256fd38466d8b4bf9272b84a556340c48fb7914
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-05 00:55:32 -0700

    Navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import './Nav.css';
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <div className="nav">
+        <img className='nav__logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png" alt="NETFLIX" />
+
+        <img className="nav__avatar" src="https://play-lh.googleusercontent.com/ZKbR3ZMjMu_Sk-BqAgWL3FwRGXSBT-TBD63y5m7bqiOvQFU4UyBDpOp1M_d6iS6zlw" alt="avatar" />
+    </div>
   )
 }
 
