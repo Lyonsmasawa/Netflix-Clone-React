4b5ef17c7bcfe5859fcf3e51a58a15316abc5a84
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-05 01:23:12 -0700

    scroll function

--- src/Nav.js
+++ src/Nav.js
@@ -6,12 +6,16 @@ function Nav() {
 
     useEffect(() => {
         window.addEventListener("scroll", () => {
-                
-        })
+            if (window.scrollY > 100) {
+                handleShow(true);
+            }else 
+                handleShow(false);
+        });
+        return () => window.removeEventListener("scroll", null);
     }, []);
 
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav__black"}`}>
         <img className='nav__logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png" alt="NETFLIX" />
 
         <img className="nav__avatar" src="https://play-lh.googleusercontent.com/ZKbR3ZMjMu_Sk-BqAgWL3FwRGXSBT-TBD63y5m7bqiOvQFU4UyBDpOp1M_d6iS6zlw" alt="avatar" />
