2bcde4b08a42402210ef5757664e77ba0500500f
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-05 01:30:42 -0700

    complete styled Navbar

--- src/Nav.js
+++ src/Nav.js
@@ -18,7 +18,7 @@ function Nav() {
     <div className={`nav ${show && "nav__black"}`}>
         <img className='nav__logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png" alt="NETFLIX" />
 
-        <img className="nav__avatar" src="https://play-lh.googleusercontent.com/ZKbR3ZMjMu_Sk-BqAgWL3FwRGXSBT-TBD63y5m7bqiOvQFU4UyBDpOp1M_d6iS6zlw" alt="avatar" />
+        <img className="nav__avatar" src="https://play-lh.googleusercontent.com/ZKbR3ZMjMu_Sk-BqAgWL3FwRGXSBT-TBD63y5m7bqiOvQFU4UyBDpOp1M_d6iS6zlw" alt="Avatar" />
     </div>
   )
 }
